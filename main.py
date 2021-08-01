3fb4e41ca031d3c81b7bddd1b60fd03b7d36b9c9
James <jed.watson.jedw@gmail.com>
2021-08-01 21:17:42 +0100

    Works with changeable center of taylor series

--- main.py
+++ main.py
@@ -5,36 +5,68 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sympy import *
-from math import factorial
+import math
+import time
 
 plt.style.use('bmh')
 fig, ax = plt.subplots()
 
-BOUND_LOWER = -1
-BOUND_UPPER = 4
-SPACING = 50
-ITER = 100
+# Plot area and detail
+X_LOWER = -10
+X_UPPER = 10
+Y_LOWER = -10
+Y_UPPER = 10
+SPACING = 200
+plt.xlim([X_LOWER, X_UPPER])
+plt.ylim([Y_LOWER, Y_UPPER])
 
-x = np.linspace(BOUND_LOWER, BOUND_UPPER, SPACING)
+# Antiderivative series
+ITER = 20
+C = 0
+
+# Plot decider
+NO_f = 0
+NO_F = 0
+
+# Set up x axis
+x = np.linspace(X_LOWER, X_UPPER, SPACING)
 
 
+# Removes a function from plot if not needed without breaking code
+def zero():
+    return [0 for _ in range(len(x))]
+
+
+# f(x). put whatever you like in here as long as it can return a real number
 def f(x):
-    return sin(x ** 2)
+    return abs(x)
 
 
+def f_plot(x):
+    return abs(x)
+
+
+# Antiderivative function. Works as explained in README.md
 def F(X):
     F = 0
     x = symbols('x')
     for n in range(ITER):
-        derivative = lambdify(x, diff(f(x), x, n))
-        F += derivative(0) / factorial(n + 1) * np.power(X, n + 1)
+        F += lambdify(x, diff(f(x), x, n))(C) / math.factorial(n + 1) * np.power(X - C, n + 1)
     return F
 
 
-y = F(x)
+# Set up y axis for both functions. replace either one with the zero() function if want to get rid
+y1 = y2 = zero()
+if not NO_f: y1 = f_plot(x)
+t1 = time.time()
+if not NO_F: y2 = F(x)
+print(f"Time taken: {t1 - time.time()} seconds")
 
-print(list(zip(x, y)))
+# Plot the points and show the graph
+ax.plot([-1000, 1000], [0, 0], color='black', linewidth=3)
+ax.plot([0, 0], [-1000, 1000], color='black', linewidth=3)
 
-ax.plot(x, y, label='F(x)')
+if not max(y1) == 0 and not min(y1) == 0: ax.plot(x, y1, label='f(x)')
+ax.plot(x, y2, label='F(x)')
 ax.legend()
 plt.show()
