ea14d343c3fc3c87ebd561a495e775547edc30ef
James <jed.watson.jedw@gmail.com>
2021-08-02 01:34:31 +0100

    LaTeX explanation done

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Antiderivative Estimator
-# Using maclaurin series
+# Using taylor polynomials
 # More detail in README.md
 
 import numpy as np
@@ -8,23 +8,27 @@ from sympy import *
 import math
 import time
 
-plt.style.use('bmh')
-fig, ax = plt.subplots()
-
 # Plot area and detail
 X_LOWER = -10
 X_UPPER = 10
 Y_LOWER = -10
 Y_UPPER = 10
-SPACING = 200
+SPACING = 1000
+
+# Set up graph
+plt.style.use('bmh')
+fig, ax = plt.subplots()
+plt.title("Antiderivative Approximation")
+plt.xlabel("x")
+plt.ylabel("y")
 plt.xlim([X_LOWER, X_UPPER])
 plt.ylim([Y_LOWER, Y_UPPER])
 
-# Antiderivative series
-ITER = 20
-C = 0
+# Increase degree of accuracy
+ACCURACY = 100
+C = -3
 
-# Plot decider
+# Does the function plot or not?
 NO_f = 0
 NO_F = 0
 
@@ -39,34 +43,36 @@ def zero():
 
 # f(x). put whatever you like in here as long as it can return a real number
 def f(x):
-    return abs(x)
+    return log(cos(x) ** 2)
 
 
+# Version of f(x) which will be able to be plotted onto the graph. Can differ from the one above
 def f_plot(x):
-    return abs(x)
+    return 2 * np.log(np.power(np.cos(x), 2))
 
 
 # Antiderivative function. Works as explained in README.md
 def F(X):
     F = 0
     x = symbols('x')
-    for n in range(ITER):
+    for n in range(ACCURACY):
         F += lambdify(x, diff(f(x), x, n))(C) / math.factorial(n + 1) * np.power(X - C, n + 1)
     return F
 
 
 # Set up y axis for both functions. replace either one with the zero() function if want to get rid
+print("Start estimation")
 y1 = y2 = zero()
 if not NO_f: y1 = f_plot(x)
 t1 = time.time()
 if not NO_F: y2 = F(x)
-print(f"Time taken: {t1 - time.time()} seconds")
+print(f"Time taken: {time.time() - t1} seconds")
 
 # Plot the points and show the graph
 ax.plot([-1000, 1000], [0, 0], color='black', linewidth=3)
 ax.plot([0, 0], [-1000, 1000], color='black', linewidth=3)
 
-if not max(y1) == 0 and not min(y1) == 0: ax.plot(x, y1, label='f(x)')
+if not y1 is zero(): ax.plot(x, y1, label='f(x)')
 ax.plot(x, y2, label='F(x)')
 ax.legend()
 plt.show()
